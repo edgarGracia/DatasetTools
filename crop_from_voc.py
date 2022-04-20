89669ed7120cd841ba5081d0b8663f00b6d47481
Edgar <edgarGrac@gmail.com>
2022-04-20 12:30:09 +0200

    Ignore missing annots

--- crop_from_voc.py
+++ crop_from_voc.py
@@ -45,7 +45,12 @@ def crop_voc(images_path: Path, annotations_path: Path, out_path: Path,
                 out_path, separate_classes, recursive)
             continue
 
-        annot_path = annotations_path.joinpath(img_path.stem + ".xml")
+        try:
+            annot_path = annotations_path.joinpath(img_path.stem + ".xml")
+        except Exception as e:
+            print(e)
+            continue
+        
         bbs = read_annotation(annot_path)
         
         img = cv2.imread(str(img_path))
