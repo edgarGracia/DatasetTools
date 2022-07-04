fd2fb244b93375da83aca88928fd081c0c154d8c
Edgar <edgarGrac@gmail.com>
2022-07-04 17:34:13 +0200

    Fix label

--- txt/visualize_bb.py
+++ txt/visualize_bb.py
@@ -1,4 +1,4 @@
-from ctypes import Union
+from typing import Union
 from pathlib import Path
 from typing import List
 from tqdm import tqdm
@@ -11,16 +11,16 @@ import matplotlib.pyplot as plt
 CV2_FONT = cv2.FONT_HERSHEY_SIMPLEX
 FONT_COLOR = (255,255,255)
 FONT_BG_COLOR = (0,0,0)
-FONT_SCALE = 2
+FONT_SCALE = 1
 FONT_THICK = 2
 
-BB_THICK = 1
+BB_THICK = 2
+BB_COLOR = (200,255,0)
 SNS_PALETTE = None
 
 
 def read_annotation(path: Path, is_xyxy: bool = False,
-    separator: str = " ", label_mapping: Union[list, None] = None
-    ) -> List[dict]:
+    separator: str = " ") -> List[dict]:
 
     lines = path.read_text().strip().split("\n")
     
@@ -38,12 +38,6 @@ def read_annotation(path: Path, is_xyxy: bool = False,
         else:
             print(f"{path} bad annotation format!!")
             return []
-        
-        if label_mapping is not None:
-            try:
-                label = label_mapping[int(label)]
-            except Exception as e:
-                print(e)
         
         if not is_xyxy:
             cx, cy, w, h = xmin, ymin, xmax, ymax
@@ -64,7 +58,8 @@ def plot_bb(images_path: Path, annotations_path: Path,
     out_path: Union[Path, None] = None, min_score: Union[float, None] = None,
     is_xyxy: bool = False, is_absolute: bool = False, separator: str = " ",
     show: bool = True, show_score: bool = True, show_label: bool = True,
-    color: Union[tuple, None] = None, label_mapping: Union[Path, None] = None):
+    color: Union[tuple, None] = None, label_mapping: Union[Path, None] = None,
+    bb_thickness: int = 2, only_labels: Union[list, None] = None):
     
     # Parse the label mapping file
     if label_mapping is not None:
@@ -79,12 +74,12 @@ def plot_bb(images_path: Path, annotations_path: Path,
         img = cv2.imread(str(img_path))
         
         # Parse annotation
-        annot_path = annotations_path.joinpath(img_path.name + ".txt")
+        annot_path = annotations_path.joinpath(img_path.stem + ".txt")
         if not annot_path.exists():
-            print(f"{annot_path} not exist!!")
+            print(f"{annot_path} does not exist!!")
             continue
         
-        bbs = read_annotation(annot_path, is_xyxy, separator, label_mapping)
+        bbs = read_annotation(annot_path, is_xyxy, separator)
         
         # Draw bounding box
         for i, bb in enumerate(bbs):
@@ -93,18 +88,35 @@ def plot_bb(images_path: Path, annotations_path: Path,
             if min_score is not None and bb["score"] is not None:
                 if bb["score"] < min_score:
                     continue
+            
+            # Filter label
+            if only_labels is not None and bb["label"] not in only_labels:
+                continue
 
             if not is_absolute:
                 xmin = int(bb["xmin"] * img.shape[1])
                 ymin = int(bb["ymin"] * img.shape[0])
                 xmax = int(min(bb["xmax"] * img.shape[1], img.shape[1]-1))
                 ymax = int(min(bb["ymax"] * img.shape[0], img.shape[0]-1))
+            else:
+                xmin = int(bb["xmin"])
+                ymin = int(bb["ymin"])
+                xmax = int(min(bb["xmax"], img.shape[1]-1))
+                ymax = int(min(bb["ymax"], img.shape[0]-1))
             
             if color is None:
-                bb_color = sns.color_palette(
-                    SNS_PALETTE,
-                    bb["label"]+1
-                )[bb["label"]]
+                try:
+                    bb_color = sns.color_palette(
+                        SNS_PALETTE,
+                        int(bb["label"])+1,
+                    )[int(bb["label"])]
+                    bb_color = [int(255 * i) for i in bb_color]
+                except Exception as e:
+                    print(e)
+                    bb_color = BB_COLOR
+            else:
+                bb_color = color
+            bb_color = tuple(reversed(bb_color))
 
             # Bounding box
             img = cv2.rectangle(
@@ -112,21 +124,25 @@ def plot_bb(images_path: Path, annotations_path: Path,
                 (xmin, ymin),
                 (xmax, ymax),
                 color=bb_color,
-                thickness=BB_THICK
+                thickness=bb_thickness
             )
 
             # Draw label
-            if show_label and show_score:
-                label = f"{bb['label']}: {round(bb['score'], 3)}"
-            elif show_label:
+            label = ""
+            if show_label:
                 label = bb["label"]
-            elif show_score:
-                label = str(round(bb["score"], 3))
-            else:
-                label = ""
+                if label_mapping is not None:
+                    try:
+                        label = label_mapping[int(bb["label"])]
+                    except Exception as e:
+                        print(e)
+            if show_score and bb['score'] is not None:
+                if show_label:
+                    label += ": "
+                label += str(round(bb['score'], 3))
             
             if label:
-                text_w, text_h, _ = cv2.getTextSize(
+                (text_w, text_h), _ = cv2.getTextSize(
                     bb["label"],
                     CV2_FONT,
                     FONT_SCALE,
@@ -184,13 +200,13 @@ if __name__ == "__main__":
         help="Annotations path"
     )
     parser.add_argument(
-        "output-path",
+        "--output-path",
         type=Path,
         default=None,
         help="Path to save the images with the bounding boxes"
     )
     parser.add_argument(
-        "min-score",
+        "--min-score",
         type=float,
         default=0.5,
         help="The minimum score the show a bounding box"
@@ -239,10 +255,26 @@ if __name__ == "__main__":
         help=("""File with a label name per line to translate labels from
             label-id to label-name""")
     )
+    parser.add_argument(
+        "--box-thickness",
+        type=int,
+        default=BB_THICK,
+        help=("Bounding Box thickness. Default 2.")
+    )
+    parser.add_argument(
+        "--only-labels",
+        help=("Specify the labels to parse separated by ','")
+    )
     
     args = parser.parse_args()
 
-    color = (int(i) for i in args.color) if args.color is not None else None
+    color = [
+        int(i) for i in args.color.split(",")
+     ] if args.color is not None else None
+
+    only_labels = [
+        i for i in args.only_labels.split(",")
+     ] if args.only_labels is not None else None
 
     plot_bb(
         images_path = args.images,
@@ -256,5 +288,7 @@ if __name__ == "__main__":
         show_score = args.show_score,
         show_label = args.show_label,
         color = color,
-        label_mapping = args.label_mapping
+        label_mapping = args.label_mapping,
+        bb_thickness =args.box_thickness,
+        only_labels = only_labels
     )
