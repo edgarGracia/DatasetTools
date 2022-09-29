53e120e11ca9c245708873c834fd9ae9d88a3294
Edgar <edgargrac@gmail.com>
2022-09-29 12:08:23 +0200

    Option to move file

--- images/copy_images_from_list.py
+++ images/copy_images_from_list.py
@@ -4,34 +4,37 @@ import argparse
 import shutil
 
 
-def copy_from_list(images_path: Path, dest_path: Path, list_file: Path, 
-    is_absolute: bool):
+def copy_from_list(source_path: Path, dest_path: Path, list_file: Path, 
+    is_absolute: bool, move: bool = False):
 
     dest_path.mkdir(exist_ok=True, parents=True)
     
-    files_to_copy = list_file.read_text().split("\n")
+    files_to_copy = list_file.read_text().splitlines()
     pbar = tqdm(files_to_copy)
     for f in pbar:
         if f:
-            src_path = str(images_path / f) if not is_absolute else f
+            src_path = str(source_path / f) if not is_absolute else f
             pbar.set_description(src_path)
-            shutil.copy(src_path, dest_path)
+            if move:
+                shutil.move(src_path, dest_path)
+            else:
+                shutil.copy(src_path, dest_path)
         
 
 if __name__ == "__main__":
-    argparser = argparse.ArgumentParser(description="""Copies the images
+    argparser = argparse.ArgumentParser(description="""Copies the files
     on a list to another directory""")
     
     argparser.add_argument(
-        "-i",
-        "--images-path",
-        help="Path to the images",
+        "-s",
+        "--source",
+        help="Source path",
         type=Path,
         required=False
     )
     argparser.add_argument(
         "-d",
-        "--dest-path",
+        "--dest",
         help="Destination path",
         type=Path,
         required=True
@@ -45,10 +48,16 @@ if __name__ == "__main__":
     )
     argparser.add_argument(
         "--is-absolute",
-        help="The list file contains the absolute path to the files to copy",
+        help="Paths on the list are absolute",
+        action="store_true"
+    )
+    argparser.add_argument(
+        "--move",
+        help="Move the files instead of copy them",
         action="store_true"
     )
 
     args = argparser.parse_args()
 
-    copy_from_list(args.images_path, args.dest_path, args.list, args.is_absolute)
+    copy_from_list(args.source, args.dest, args.list,
+        args.is_absolute, args.move)
